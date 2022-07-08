a2acdfff5efee5374dd02dc2a198855b1dbae57a
dev-vikram <vikram.jagtap@webfry.in>
2022-07-08 13:59:25 +0530

    final home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,43 +11,53 @@ export default function Footer(){
                     </div>
                     
                 </div>
-                <div className="bg-[#A82C2B] px-28 flex gap-14 justify-center pt-10">
-                    <div className=" space-y-5 ">
-                        <img className="h-14" src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fnikahlogolg.svg&w=384&q=75" alt="" />
-                        <p className="w-96 text-white text-sm text-justify">We at DreamNikah are here to make sure you find your better half, your dream partner, and make it worth experiencing. Our sole purpose is your ease and trust. Bringing together happy hearts with a promise to the brothers and sisters of this Ummah. Be a part of DreamNikah today, and welcome new beginnings</p>
+                <div className="px-28 bg-[#A82C2B]">
+                    <div className="  flex gap-14 justify-center pt-10">
+                        <div className=" space-y-5 ">
+                            <img className="h-14" src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fnikahlogolg.svg&w=384&q=75" alt="" />
+                            <p className="w-96 text-white text-sm text-justify">We at DreamNikah are here to make sure you find your better half, your dream partner, and make it worth experiencing. Our sole purpose is your ease and trust. Bringing together happy hearts with a promise to the brothers and sisters of this Ummah. Be a part of DreamNikah today, and welcome new beginnings</p>
+                        </div>
+                        <div className="text-white">
+                            <p className="text-2xl font-bold">Menus</p>
+                            <ul className="space-y-5 mt-8">
+                                <li>Home</li>
+                                <li>About Us</li>
+                                <li>Contact</li>
+                                <li>Services</li>
+                            </ul>
+                        </div>
+                        <div className="text-white">
+                            <p className="text-2xl font-bold">Quick Link</p>
+                            <ul className="space-y-5 mt-8">
+                                <li>Wedding Vendors</li>
+                                <li>Affiliate Registration</li>
+                                <li>Privacy Policy</li>
+                                <li>Term of Services</li>
+                                <li>FAQ</li>
+                            </ul>
+                        </div>
+                        <div className="text-white">
+                            <p className="text-2xl font-bold">Social Media</p>
+                            <p className="w-80 text-justify mt-8">
+                            Follow us on Instagram, Facebook, Twitter and be a part of our social networking family. Get day to day updates and savor the advantage of pins and posts concerning marriages and much more.
+                            </p>
+                            <ul className="flex gap-10 mt-10">
+                                <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Ffb_icon.svg&w=32&q=75" alt="" /></li>
+                                <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fig_icon.svg&w=32&q=75" alt="" /></li>
+                                <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Ftweetr_icon.svg&w=32&q=75" alt="" /></li>
+                                <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fyt_icon.svg&w=32&q=75" alt="" /></li>
+                            </ul>
+                        </div>
+                        
                     </div>
-                    <div className="text-white">
-                        <p className="text-2xl font-bold">Menus</p>
-                        <ul className="space-y-5 mt-8">
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>Contact</li>
-                            <li>Services</li>
-                        </ul>
+                    <div className="flex justify-between container mx-auto text-white border-t-2 py-2 mt-5">
+                        <p>By Dream Nikah</p>
+                        <p>Copyright @2022 DreamNikha</p>
                     </div>
-                    <div className="text-white">
-                        <p className="text-2xl font-bold">Quick Link</p>
-                        <ul className="space-y-5 mt-8">
-                            <li>Wedding Vendors</li>
-                            <li>Affiliate Registration</li>
-                            <li>Privacy Policy</li>
-                            <li>Term of Services</li>
-                            <li>FAQ</li>
-                        </ul>
-                    </div>
-                    <div className="text-white">
-                        <p className="text-2xl font-bold">Social Media</p>
-                        <p className="w-80 text-justify mt-8">
-                        Follow us on Instagram, Facebook, Twitter and be a part of our social networking family. Get day to day updates and savor the advantage of pins and posts concerning marriages and much more.
-                        </p>
-                        <ul className="flex gap-10 mt-10">
-                            <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Ffb_icon.svg&w=32&q=75" alt="" /></li>
-                            <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fig_icon.svg&w=32&q=75" alt="" /></li>
-                            <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Ftweetr_icon.svg&w=32&q=75" alt="" /></li>
-                            <li><img src="https://staging.dreamnikah.com/_next/image?url=%2Fimages%2Fyt_icon.svg&w=32&q=75" alt="" /></li>
-                        </ul>
-                    </div>
+                   
                 </div>
+                
+                
             </div>
         </>
     );
